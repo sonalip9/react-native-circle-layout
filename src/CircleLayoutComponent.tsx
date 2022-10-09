8dc31ede1650307baf9d4106c0fd88211bc626c6
Sonali Parekh <sonali.parekh912@gmail.com>
2022-10-09 22:16:03 +0530

    chore: add path alias

--- src/CircleLayoutComponent.tsx
+++ src/CircleLayoutComponent.tsx
@@ -1,8 +1,9 @@
 import React, { ReactNode, useMemo } from 'react';
 import { Animated } from 'react-native';
 
-import { useAnimation } from './hooks';
 import { circleComponentStyles } from './styles';
+
+import { useAnimation } from '@hooks';
 
 type ComponentProps = {
   /**
