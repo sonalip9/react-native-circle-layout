fa2c27e89f06eb46bd9890aae15ba19731599184
Sonali <sonali.parekh912@gmail.com>
2022-10-13 01:01:00 +0530

    chore: update example app

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -14,7 +14,11 @@ import { styles } from './styles';
 
 const App = () => {
   const [showInitial, setShowInitial] = React.useState(false);
-  const { width: windowWidth } = useWindowDimensions();
+  const { width: windowWidth, height: windowHeight } = useWindowDimensions();
+  const viewFlexDirection = React.useMemo(
+    () => (windowHeight > windowWidth ? 'column' : 'row'),
+    [windowHeight, windowWidth]
+  );
   const [showCircle, setShowCircle] = React.useState(false);
   const [radius, setRadius] = React.useState(100);
   const [sweepAngle, setSweepAngle] = React.useState(2 * Math.PI);
@@ -38,86 +42,90 @@ const App = () => {
   return (
     <SafeAreaView style={styles.container}>
       <StatusBar />
-      <View style={styles.container}>
-        {showInitial && (
-          <CircleLayout
-            animationConfig={{
-              gap: 1000,
-              duration: 1000,
+      <View style={[styles.container, { flexDirection: viewFlexDirection }]}>
+        <View style={styles.flex}>
+          {showInitial && (
+            <CircleLayout
+              animationConfig={{
+                gap: 1000,
+                duration: 1000,
+              }}
+              centerComponent={<View style={styles.centerComponent} />}
+              components={createComponents(numberOfPoints)}
+              containerStyle={styles.circleLayoutContainer}
+              radius={radius}
+              showComponents={showCircle}
+              startAngle={startAngle}
+              sweepAngle={sweepAngle}
+            />
+          )}
+        </View>
+        {windowHeight > windowWidth && <View style={styles.flex} />}
+        <View style={styles.flex}>
+          <View style={styles.sliderContainer}>
+            <Text style={styles.sliderLabel}>Radius</Text>
+            <Slider
+              maximumValue={Number(((windowWidth * 0.85) / 2).toFixed(0))}
+              minimumValue={0}
+              onValueChange={setRadius}
+              step={1}
+              style={styles.slider}
+              value={radius}
+            />
+            <Text style={styles.sliderValue}>{radius}</Text>
+          </View>
+          <View style={styles.sliderContainer}>
+            <Text style={styles.sliderLabel}>Sweep Angle</Text>
+            <Slider
+              maximumValue={2 * Math.PI}
+              minimumValue={0}
+              onValueChange={setSweepAngle}
+              style={styles.slider}
+              value={sweepAngle}
+            />
+            <Text style={styles.sliderValue}>
+              {((sweepAngle * 180) / Math.PI).toFixed(2)}°
+            </Text>
+          </View>
+          <View style={styles.sliderContainer}>
+            <Text style={styles.sliderLabel}>Start Angle</Text>
+            <Slider
+              maximumValue={2 * Math.PI}
+              minimumValue={0}
+              onValueChange={setStartAngle}
+              style={styles.slider}
+              value={startAngle}
+            />
+            <Text style={styles.sliderValue}>
+              {((startAngle * 180) / Math.PI).toFixed(2)}°
+            </Text>
+          </View>
+          <View
+            pointerEvents={showInitial ? 'none' : 'auto'}
+            style={styles.sliderContainer}
+          >
+            <Text style={styles.sliderLabel}>Number of Points</Text>
+            <Slider
+              disabled={showInitial}
+              maximumValue={20}
+              minimumValue={2}
+              onValueChange={setNumberOfPoints}
+              step={1}
+              style={styles.slider}
+              value={numberOfPoints}
+            />
+            <Text style={styles.sliderValue}>{numberOfPoints}</Text>
+          </View>
+
+          <Button
+            onPress={() => {
+              setShowInitial(() => true);
+              setShowCircle((oldValue) => !oldValue);
             }}
-            centerComponent={<View style={styles.centerComponent} />}
-            components={createComponents(numberOfPoints)}
-            containerStyle={styles.circleLayoutContainer}
-            radius={radius}
-            showComponents={showCircle}
-            startAngle={startAngle}
-            sweepAngle={sweepAngle}
+            title={showCircle ? 'Hide circle layout' : 'Show circle layout'}
           />
-        )}
-        <View style={styles.flex} />
-
-        <View style={styles.sliderContainer}>
-          <Text style={styles.sliderLabel}>Radius</Text>
-          <Slider
-            maximumValue={Number(((windowWidth * 0.85) / 2).toFixed(0))}
-            minimumValue={0}
-            onValueChange={setRadius}
-            step={1}
-            style={styles.slider}
-            value={radius}
-          />
-          <Text style={styles.sliderValue}>{radius}</Text>
+          {windowHeight > windowWidth && <View style={styles.footer} />}
         </View>
-        <View style={styles.sliderContainer}>
-          <Text style={styles.sliderLabel}>Sweep Angle</Text>
-          <Slider
-            maximumValue={2 * Math.PI}
-            minimumValue={0}
-            onValueChange={setSweepAngle}
-            style={styles.slider}
-            value={sweepAngle}
-          />
-          <Text style={styles.sliderValue}>
-            {((sweepAngle * 180) / Math.PI).toFixed(2)}°
-          </Text>
-        </View>
-        <View style={styles.sliderContainer}>
-          <Text style={styles.sliderLabel}>Start Angle</Text>
-          <Slider
-            maximumValue={2 * Math.PI}
-            minimumValue={0}
-            onValueChange={setStartAngle}
-            style={styles.slider}
-            value={startAngle}
-          />
-          <Text style={styles.sliderValue}>
-            {((startAngle * 180) / Math.PI).toFixed(2)}°
-          </Text>
-        </View>
-        <View
-          pointerEvents={showInitial ? 'none' : 'auto'}
-          style={styles.sliderContainer}
-        >
-          <Text style={styles.sliderLabel}>Number of Points</Text>
-          <Slider
-            maximumValue={20}
-            minimumValue={2}
-            onValueChange={setNumberOfPoints}
-            step={1}
-            style={styles.slider}
-            value={numberOfPoints}
-          />
-          <Text style={styles.sliderValue}>{numberOfPoints}</Text>
-        </View>
-
-        <Button
-          onPress={() => {
-            setShowInitial(() => true);
-            setShowCircle((oldValue) => !oldValue);
-          }}
-          title={showCircle ? 'Hide circle layout' : 'Show circle layout'}
-        />
-        <View style={styles.footer} />
       </View>
     </SafeAreaView>
   );
