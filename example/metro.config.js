18c741b65bbe1c5850e64f62082c230a0f475c5a
Sonali Parekh <sonali.parekh912@gmail.com>
2026-05-28 00:02:55 +0530

    refactor: simplify babel.config.js and enhance metro.config.js with pnpm temp path handling

--- example/metro.config.js
+++ example/metro.config.js
@@ -3,6 +3,8 @@ const { getDefaultConfig } = require('@expo/metro-config');
 const { withMetroConfig } = require('react-native-monorepo-config');
 
 const root = path.resolve(__dirname, '..');
+const pnpmTempPathPattern =
+  /[/\\]node_modules[/\\](?:\.pnpm[/\\][^/\\]+[/\\]node_modules[/\\])?[^/\\]+_tmp_\d+_\d+(?:[/\\].*)?$/;
 
 /**
  * Metro configuration
@@ -15,5 +17,10 @@ const config = withMetroConfig(getDefaultConfig(__dirname), {
   dirname: __dirname,
   workspaces: ['.', 'example'],
 });
+
+config.resolver.blockList = [
+  ...[config.resolver.blockList].flat().filter(Boolean),
+  pnpmTempPathPattern,
+];
 
 module.exports = config;
