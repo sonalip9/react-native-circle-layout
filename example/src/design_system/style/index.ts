6f6521de7b2481702a4c764904438d0a7b515b46
Sonali Parekh <sonali.parekh912@gmail.com>
2026-05-31 11:25:40 +0530

    feat(theme): enhance theme structure by adding border radii and text variants

--- example/src/design_system/style/index.ts
+++ example/src/design_system/style/index.ts
@@ -17,10 +17,22 @@ const theme = createTheme({
   breakpoints: {},
   colors: palette,
   spacing: {
-    l: 24,
-    m: 16,
     s: 8,
+    m: 16,
+    l: 24,
     xl: 40,
+  },
+  borderRadii: {
+    s: 4,
+    m: 8,
+    l: 16,
+    xl: 24,
+  },
+  textVariants: {
+    defaults: {
+      color: 'black',
+      fontSize: 16,
+    },
   },
 });
 
