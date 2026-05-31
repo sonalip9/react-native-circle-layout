0bfe544ae2488932d7088be2c56c1481b181d9fb
Sonali Parekh <sonali.parekh912@gmail.com>
2026-05-31 11:26:54 +0530

    feat(button): refactor Button component and integrate buttonVariants into theme

--- example/src/design_system/atoms/Button/Button.tsx
+++ example/src/design_system/atoms/Button/Button.tsx
@@ -1,20 +1,64 @@
-import type { BoxProps } from '@shopify/restyle';
 import {
-  Button as ReactNativeButton,
-  type ButtonProps as ReactNativeButtonProps,
-} from 'react-native';
+  backgroundColor,
+  type BackgroundColorProps,
+  border,
+  type BorderProps,
+  createRestyleComponent,
+  createVariant,
+  spacing,
+  type SpacingProps,
+  type VariantProps,
+} from '@shopify/restyle';
+import { TouchableOpacity, type ViewStyle } from 'react-native';
 
-import { View } from '../View';
 import type { Theme } from '../../style';
+import { Text } from '../Text';
+import { View, type ViewProps } from '../View';
 
-export type ButtonProps = {
-  containerStyle: BoxProps<Theme>;
-} & ReactNativeButtonProps;
+type RestyleProps = SpacingProps<Theme> &
+  BorderProps<Theme> &
+  BackgroundColorProps<Theme>;
+type ResolvedStyle = ViewStyle & { color?: string };
 
-const Button = ({ containerStyle, ...buttonProps }: ButtonProps) => (
-  <View {...containerStyle}>
-    <ReactNativeButton {...buttonProps} />
-  </View>
+type Props = {
+  onPress: () => void;
+  label: string;
+};
+
+type ButtonComponentProps = Omit<ViewProps, 'style'> &
+  Props & { style?: [ResolvedStyle] };
+
+const ButtonComponent = ({
+  onPress,
+  label,
+  style,
+  ...props
+}: ButtonComponentProps) => {
+  const [{ color: labelColor, ...viewStyle }] = style ?? [{}];
+
+  return (
+    <TouchableOpacity onPress={onPress}>
+      <View style={viewStyle as ViewStyle} {...props}>
+        <Text style={labelColor ? { color: labelColor } : undefined}>
+          {label}
+        </Text>
+      </View>
+    </TouchableOpacity>
+  );
+};
+
+export type ButtonProps = VariantProps<Theme, 'buttonVariants'> &
+  RestyleProps &
+  Props;
+
+const Button = createRestyleComponent<ButtonProps, Theme>(
+  [
+    spacing,
+    border,
+    backgroundColor,
+    createVariant({ themeKey: 'buttonVariants' }),
+  ],
+  ButtonComponent
 );
 
 export default Button;
--- example/src/design_system/style/index.ts
+++ example/src/design_system/style/index.ts
@@ -11,6 +11,29 @@ const palette = {
   purpleLight: '#8C6FF7',
   purplePrimary: '#5A31F4',
   white: '#F0F2F3',
+  transparent: 'transparent',
+};
+
+const buttonVariants = {
+  defaults: {
+    backgroundColor: 'purpleDark',
+    padding: 'm',
+    borderRadius: 'm',
+    alignItems: 'center',
+    justifyContent: 'center',
+    color: 'white',
+  },
+  text: {
+    backgroundColor: 'transparent',
+    color: 'purpleDark',
+  },
+  outlined: {
+    backgroundColor: 'transparent',
+    borderWidth: 1,
+    borderColor: 'purpleDark',
+    color: 'purpleDark',
+  },
+  contained: {},
 };
 
 const theme = createTheme({
@@ -34,6 +57,7 @@ const theme = createTheme({
       fontSize: 16,
     },
   },
+  buttonVariants,
 });
 
 export type Theme = typeof theme;
