e64b26f2787aa93d7045cca72fd6bfefcf0afcdc
Sonali Parekh <sonali.parekh912@gmail.com>
2026-05-31 22:11:45 +0530

    feat(switch): add Switch component with customizable labels and state management

--- example/src/design_system/molecules/index.ts
+++ example/src/design_system/molecules/index.ts
@@ -3,3 +3,5 @@ export type { DropdownOption, DropdownProps } from './Dropdown';
 export { PopUp } from './PopUp';
 export { SliderWithLabel } from './SliderWithLabel';
 export type { SliderWithLabelProps } from './SliderWithLabel';
+export { Switch } from './Switch';
+export type { SwitchProps } from './Switch';
